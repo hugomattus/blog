06307e167e625d1b0a5feb1b6c2fb08105dd8929
hugomllo <hugomattusdd@gmail.com>
2022-05-03 12:45:18 -0300

    Creat the structure of components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 
+import Header from './components/Article';
+
+import Article from './components/Article';
+
+
 
 function App() {
   return (
-    <h1>Meu Blog</h1>
+    <>
+    <Header></Header>
+    <Article></Article>
+    </>
   );
 }
 
